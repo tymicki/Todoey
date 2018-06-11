854c45de4d9004efd70d818ac72f2bbdb7a807f3
Jacek Tymicki <jtymicki@fb.com>
2018-06-11 14:40:25 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jacek Tymicki on 6/11/18.
+//  Copyright © 2018 Jacek Tymicki. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
